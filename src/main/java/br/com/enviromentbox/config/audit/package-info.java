097320663d83af3ef95373229171af4d5d03a695
Lucas Silvestre Kloss Teles <klossteles@gmail.com>
2017-05-01 14:00:48 -0300

    Iníco Projeto

---
+++ src/main/java/br/com/enviromentbox/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package br.com.enviromentbox.config.audit;
---
+++ src/main/java/br/com/enviromentbox/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package br.com.enviromentbox.repository;
+
+import br.com.enviromentbox.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
