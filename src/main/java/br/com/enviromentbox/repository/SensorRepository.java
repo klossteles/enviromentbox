9a554ad3ff7acf41764db60629cf7e201e7ddc39
Lucas Silvestre Kloss Teles <klossteles@gmail.com>
2017-06-06 21:49:57 -0300

    Correções API

--- src/main/java/br/com/enviromentbox/repository/SensorRepository.java
+++ src/main/java/br/com/enviromentbox/repository/SensorRepository.java
@@ -13,8 +13,8 @@ import java.util.ArrayList;
 @SuppressWarnings("unused")
 public interface SensorRepository extends JpaRepository<Sensor,Long> {
 
-    @Query(value = "select s.id, ts.nome, s.id_device_id FROM sensor s JOIN tipo_sensor ts ON ts.id = s.id_tipo_sensor WHERE s.id_device_id = :device_id",nativeQuery = true)
-    ArrayList<Sensor> consultarSensoresByDeviceId(@Param("device_id") Long device_id);
+    @Query(value = "select s.id as sensor_id, ts.nome as sensor_nome, s.id_device_id as device_id FROM sensor s JOIN tipo_sensor ts ON ts.id = s.id_tipo_sensor WHERE s.id_device_id = :device_id",nativeQuery = true)
+    ArrayList<Object[]> consultarSensoresByDeviceId(@Param("device_id") Long device_id);
 
     @Query(value = "SELECT s.id, ts.nome as nome_sensor, s.id_device_id, d.nome as nome_device, ts.id as id_tipo_sensor FROM sensor s JOIN device d ON d.id = s.id_device_id JOIN tipo_sensor ts ON ts.id = s.id_tipo_sensor",nativeQuery = true)
     ArrayList<Object[]> consultarSensores();
