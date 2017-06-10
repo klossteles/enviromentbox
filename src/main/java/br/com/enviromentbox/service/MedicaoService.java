e1bb0fcf71abbdbe460262360fd9d4945e30620e
pietrangelo <pietrangelo@alunos.utfpr.edu.br>
2017-06-10 15:01:25 -0300

    Correção consultaMedicaoByDeviceId

--- src/main/java/br/com/enviromentbox/service/MedicaoService.java
+++ src/main/java/br/com/enviromentbox/service/MedicaoService.java
@@ -1,16 +1,12 @@
 package br.com.enviromentbox.service;
 
-import br.com.enviromentbox.domain.Medicao;
-
-import java.util.List;
-
 /**
  * Created by Kloss Teles on 16/05/2017.
  */
 public interface MedicaoService {
     void salvarStr(String str);
 
-    List<Medicao> consultaMedicoesByDeviceId(Long device_id);
+    String consultaMedicoesByDeviceId(Long device_id);
 
     String consultarMedicoesFiltradas(Long device_id, Long sensor_id, String data_inicial, String data_final);
 
--- src/main/java/br/com/enviromentbox/service/MedicaoServiceImpl.java
+++ src/main/java/br/com/enviromentbox/service/MedicaoServiceImpl.java
@@ -129,8 +129,10 @@ public class MedicaoServiceImpl implements MedicaoService {
     }
 
     @Override
-    public List<Medicao> consultaMedicoesByDeviceId(Long device_id) {
-        return medicaoRepository.consultaMedicoesByDeviceId(device_id);
+    public String consultaMedicoesByDeviceId(Long device_id) {
+        List<Object[]> medicoes = medicaoRepository.consultaMedicoesByDeviceId(device_id);
+        JSONArray jsonArray = new JSONArray(medicoes);
+        return jsonArray.toString();
     }
 
     @Override
